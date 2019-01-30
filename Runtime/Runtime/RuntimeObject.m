cf6ff0690c9b854ce92fa5e67c5d75266bac6ad6
g623171541 <Paddygu@feelwx.com>
2019-01-30 22:45:10 +0800

    注释

--- Runtime/Runtime/RuntimeObject.m
+++ Runtime/Runtime/RuntimeObject.m
@@ -41,7 +41,7 @@
     // 如果是test方法 打印日志
     if (aSelector == @selector(test)) {
         NSLog(@"methodSignatureForSelector");
-        // 如果是test方法，则返回一个正确的方法签名
+        // 如果是test方法，则返回一个正确的方法签名，Runtime就会创建一个NSInvocation 对象并发送 -forwardInvocation:消息给目标对象。
         return [NSMethodSignature signatureWithObjCTypes:"v@:"];
         /*  方法签名的说明：
             v 代表返回值是void类型的；
@@ -49,6 +49,7 @@
             : 代表第二个参数类型是SEL，即@selector(test)
          */
         
+        // 如果此方法 return nil; 则会调用 doesNotRecognizeSelector: 程序就这样挂掉了
     }else{
         // 返回父类的默认调用
         return [super methodSignatureForSelector:aSelector];
